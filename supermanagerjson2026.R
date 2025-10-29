efaf55ddce348acbedd3bfb5214280b2940a8789
Iván Villanueva Sabalete <villanueva_i1@hotmail.com>
2025-10-29 13:54:50 +0100

    Update supermanagerjson2026.R

--- supermanagerjson2026.R
+++ supermanagerjson2026.R
@@ -39,3 +39,40 @@ superManager <-
 
 # guardarlo en csv
 write.csv(superManager, "data/supermanager_juagadores_2026.csv")
+
+#funcion estadisticas por jugador
+players_superM <- function(id) {
+  
+  superManager_player <- fromJSON(content(GET(paste0(Sys.getenv("URL_PLAYERS"), id),
+                                              add_headers(.headers = headers)),
+                                          "text", encoding = "UTF-8"))
+  
+  playerDF <- superManager_player %>%
+    pluck("playerStats") %>%
+    tibble() %>%
+    mutate(
+      shortName = superManager_player$shortName,
+      nameTeam = superManager_player$nameTeam,
+      license = superManager_player$license,
+      photo = superManager_player$photo2,
+      initialPrice = superManager_player$initialPrice,
+      price = superManager_player$price,
+      nick = superManager_player$nick,
+      idTeam = superManager_player$idTeam,
+      number = superManager_player$number,
+      idPlayer = superManager_player$idPlayer 
+    ) %>%
+    filter(!is.na(idJourney)) %>% 
+    select(idPlayer, shortName, nick, license, idTeam, nameTeam, playerPrice, initialPrice, price, everything()) %>%
+    select(where(~ !all(is.na(.x))))
+ 
+  return(playerDF)
+}
+
+# Map the function over the list of ids
+players_superM_Df <- map_df(superManager$id, players_superM)
+
+
+write.csv(players_superM_Df,"data/supermanager_juagadores_stats_2026.csv"))
+
+
